c104d888993ca342b9fe540dbe5052f6cd70c669
Sinanhubb <jiaa22ji@gmail.com>
2025-05-15 14:29:39 +0530

    new page added

--- my-portfolio/src/animations.js
+++ my-portfolio/src/animations.js
@@ -1,22 +1,14 @@
-// animations.js
 export const fadeInUp = {
-  hidden: { opacity: 0, y: 40 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.6,
-      ease: [0.6, 0.05, -0.01, 0.9]
+    hidden: { opacity: 0, y: 40 },
+    visible: { opacity: 1, y: 0 }
+  };
+  
+  export const staggerContainer = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.2
+      }
     }
-  }
-};
-
-export const staggerContainer = {
-  hidden: {},
-  visible: {
-    transition: {
-      staggerChildren: 0.2,
-      delayChildren: 0.3
-    }
-  }
-};
+  };
+  
