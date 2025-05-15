c097938acb6396776f8403a34190878143285a82
Sinanhubb <jiaa22ji@gmail.com>
2025-05-15 11:28:43 +0530

    updation

--- my-portfolio/src/PrivateRoute.jsx
+++ my-portfolio/src/PrivateRoute.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { isValidToken } from './utils/auth';
 
-export const PrivateRoute = () => {
+const PrivateRoute = () => {
   const location = useLocation();
   const token = localStorage.getItem('admin_token');
-  
+
   if (!isValidToken(token)) {
     localStorage.removeItem('admin_token');
     return <Navigate to="/admin-login" state={{ from: location }} replace />;
   }
-  
+
   return <Outlet />;
 };
+
+export default PrivateRoute;
